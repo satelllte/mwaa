5bff3154fa7d72b98c7c3e27035b254978de566f
George Satellite <lunaerxs@gmail.com>
2023-04-02 00:01:43 +0300

    base context: rename internal vars

--- src/MockBaseAudioContext.ts
+++ src/MockBaseAudioContext.ts
@@ -34,20 +34,20 @@ export class MockBaseAudioContext implements Omit<BaseAudioContext,
 		return this._sampleRate
 	}
 
-	private static _defaultSampleRate: number = 44100
-	private static _minSampleRate: number = 3000 // https://chromium.googlesource.com/chromium/blink/+/refs/heads/main/Source/platform/audio/AudioUtilities.cpp -> minAudioBufferSampleRate
-	private static _maxSampleRate: number = 192000 // https://chromium.googlesource.com/chromium/blink/+/refs/heads/main/Source/platform/audio/AudioUtilities.cpp -> maxAudioBufferSampleRate
+	private static _sampleRateDefault: number = 44100
+	private static _sampleRateMin: number = 3000 // https://chromium.googlesource.com/chromium/blink/+/refs/heads/main/Source/platform/audio/AudioUtilities.cpp -> minAudioBufferSampleRate
+	private static _sampleRateMax: number = 192000 // https://chromium.googlesource.com/chromium/blink/+/refs/heads/main/Source/platform/audio/AudioUtilities.cpp -> maxAudioBufferSampleRate
 
-	protected _sampleRate: number = MockBaseAudioContext._defaultSampleRate
+	protected _sampleRate: number = MockBaseAudioContext._sampleRateDefault
 
-	protected constructor(sampleRate: number = MockBaseAudioContext._defaultSampleRate) {
+	protected constructor(sampleRate: number = MockBaseAudioContext._sampleRateDefault) {
 		if (new.target === MockBaseAudioContext) {
 			throw new TypeError('Illegal constructor')
 		}
 
-		if (sampleRate < MockBaseAudioContext._minSampleRate || sampleRate > MockBaseAudioContext._maxSampleRate) {
+		if (sampleRate < MockBaseAudioContext._sampleRateMin || sampleRate > MockBaseAudioContext._sampleRateMax) {
 			const targetName: string = new.target.name.replace('Mock', '')
-			throw new DOMException(`Failed to construct '${targetName}': The sample rate provided (${sampleRate}) is outside the range [${MockBaseAudioContext._minSampleRate}, ${MockBaseAudioContext._maxSampleRate}]`)
+			throw new DOMException(`Failed to construct '${targetName}': The sample rate provided (${sampleRate}) is outside the range [${MockBaseAudioContext._sampleRateMin}, ${MockBaseAudioContext._sampleRateMax}]`)
 		}
 
 		this._sampleRate = sampleRate
