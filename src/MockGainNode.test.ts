139fabe9b0f7f40a2fe8d33ac7edc403619383c1
George Satellite <lunaerxs@gmail.com>
2023-04-09 22:20:47 +0300

    AudioParam reusable test

--- src/MockGainNode.test.ts
+++ src/MockGainNode.test.ts
@@ -6,8 +6,6 @@ import {MockGainNode} from './MockGainNode'
 import {testAudioNode} from './utils/testing/testAudioNode'
 
 describe('MockGainNode', () => {
-	const consoleWarnSpy: SpyInstance = useConsoleWarnSpy()
-
 	beforeAll(() => {
 		MWAA.mock()
 	})
@@ -40,11 +38,10 @@ describe('MockGainNode', () => {
 	testAudioParam({
 		nodeName: 'GainNode',
 		paramName: 'gain',
-		creator() {
-			const ctx: AudioContext = new AudioContext()
+		creator(ctx: BaseAudioContext) {
 			const node: GainNode = new GainNode(ctx)
 			const	param: AudioParam = node.gain
-			return {ctx, node, param}
+			return {node, param}
 		},
 		expectedAutomationRate: 'a-rate',
 		expectedValue: 1,
@@ -52,73 +49,16 @@ describe('MockGainNode', () => {
 		expectedMinValue: -3.4028234663852886e+38,
 		expectedMaxValue: 3.4028234663852886e+38,
 	})
-
-	/**
-	 * Tests for: "gain" AudioParam
-	 */
-	describe('"gain" AudioParam', () => {
-		describe('"value" property', () => {
-			it('can be set from constructor', () => {
-				const ctx: AudioContext = new AudioContext()
-				const gainNode: GainNode = new GainNode(ctx, {gain: 0.77})
-				expect(gainNode.gain.value).toEqual(0.77)
-			})
-
-			it('clamps the value if it is out of range from constructor', () => {
-				const ctx: AudioContext = new AudioContext()
-				expect(consoleWarnSpy).toHaveBeenCalledTimes(0)
-				const gainNode: GainNode = new GainNode(ctx, {gain: -3.4028234663852886e+38 - 1e38})
-				expect(consoleWarnSpy).toHaveBeenCalledTimes(1)
-				expect(consoleWarnSpy).toHaveBeenCalledWith('value -4.4028234663852884e+38 outside nominal range [-3.4028234663852886e+38, 3.4028234663852886e+38]; value will be clamped')
-				expect(gainNode.gain.value).toEqual(-3.4028234663852886e+38)
-			})
-
-			it('throws error if it is wrong from constructor', () => {
-				const ctx: AudioContext = new AudioContext()
-				expect(() =>
-					// @ts-expect-error bad input test
-					new GainNode(ctx, {gain: 'x'}),
-				).toThrowErrorMatchingInlineSnapshot('"The provided float value is non-finite"')
-			})
-
-			it('can be updated from setter', () => {
-				const ctx: AudioContext = new AudioContext()
-				const gainNode: GainNode = new GainNode(ctx)
-				expect(gainNode.gain.value).toEqual(1)
-				gainNode.gain.value = 0.75
-				expect(gainNode.gain.value).toEqual(0.75)
-			})
-
-			it('clamps the value if it is out of range from setter', () => {
-				const ctx: AudioContext = new AudioContext()
-				const gainNode: GainNode = new GainNode(ctx)
-				expect(gainNode.gain.value).toEqual(1)
-				expect(consoleWarnSpy).toHaveBeenCalledTimes(0)
-				gainNode.gain.value = -3.4028234663852886e+38 - 1e38
-				expect(consoleWarnSpy).toHaveBeenCalledTimes(1)
-				expect(consoleWarnSpy).toHaveBeenCalledWith('value -4.4028234663852884e+38 outside nominal range [-3.4028234663852886e+38, 3.4028234663852886e+38]; value will be clamped')
-				expect(gainNode.gain.value).toEqual(-3.4028234663852886e+38)
-			})
-
-			it('throws error if it is wrong from setter', () => {
-				const ctx: AudioContext = new AudioContext()
-				const gainNode: GainNode = new GainNode(ctx)
-				expect(gainNode.gain.value).toEqual(1)
-				expect(() => {
-					// @ts-expect-error bad input test
-					gainNode.gain.value = 'x'
-				}).toThrowErrorMatchingInlineSnapshot('"Failed to set the \'value\' property on \'AudioParam\': The provided float value is non-finite."')
-			})
-		})
-	})
 })
 
+// eslint-disable-next-line no-warning-comments
+// TODO: move to utils/testing
+
 type CreatorFnResult = {
-	ctx: BaseAudioContext;
 	node: AudioNode;
 	param: AudioParam;
 }
-type CreatorFn = () => CreatorFnResult
+type CreatorFn = (ctx: BaseAudioContext) => CreatorFnResult
 
 const testAudioParam = ({
 	nodeName,
@@ -142,19 +82,27 @@ const testAudioParam = ({
 	describe(`${nodeName}.${paramName}`, () => {
 		const consoleWarnSpy: SpyInstance = useConsoleWarnSpy()
 
+		// Operating at 25% of the value shifts (e.g., instead of +/- 1) allows the tests to be also reliable for huge minValue/maxValue ranges like GainNode.gain param has
+		const differentInRangeValue: number = ((expectedMaxValue - expectedMinValue) * 0.25) + expectedMinValue // Taking value at 25% in-between of the param's min/max range
+		const lessThanMinValue: number = expectedMinValue - (Math.abs(expectedMinValue) * 0.25) // Taking value 25% less than min relative to the param's min/max range
+		const moreThanMaxValue: number = expectedMaxValue + (Math.abs(expectedMaxValue) * 0.25) // Taking value 25% more than max relative to the param's min/max range
+
 		it('is instance of AudioParam', () => {
-			const {ctx, node, param}: CreatorFnResult = creator()
+			const ctx: AudioContext = new AudioContext()
+			const {node, param}: CreatorFnResult = creator(ctx)
 			expect(param).toBeInstanceOf(AudioParam)
 		})
 
 		describe('"automationRate" property', () => {
 			it(`equals to expectedAutomationRate (${expectedAutomationRate}) by default`, () => {
-				const {ctx, node, param}: CreatorFnResult = creator()
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
 				expect(param.automationRate).toEqual(expectedAutomationRate)
 			})
 
 			it('can be updated via setter', () => {
-				const {ctx, node, param}: CreatorFnResult = creator()
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
 				expect(param.automationRate).toEqual(expectedAutomationRate)
 				const flippedAutomationRate: AutomationRate = expectedAutomationRate === 'a-rate' ? 'k-rate' : 'a-rate'
 				param.automationRate = flippedAutomationRate
@@ -162,7 +110,8 @@ const testAudioParam = ({
 			})
 
 			it('does not update & logs warning if received wrong value via setter', () => {
-				const {ctx, node, param}: CreatorFnResult = creator()
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
 				expect(param.automationRate).toEqual(expectedAutomationRate)
 				expect(consoleWarnSpy).toHaveBeenCalledTimes(0)
 				// @ts-expect-error bad input test
@@ -173,51 +122,135 @@ const testAudioParam = ({
 			})
 		})
 
-		// eslint-disable-next-line no-warning-comments
-		// TODO: create reusable test
-		describe.todo('"value" property')
+		describe('"value" property', () => {
+			it(`equals to expectedValue ${expectedValue}`, () => {
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
+				expect(param.value).toEqual(expectedValue)
+			})
+
+			it('can be updated from setter', () => {
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
+				expect(param.value).toEqual(expectedValue)
+				param.value = differentInRangeValue
+				expect(param.value).toEqual(differentInRangeValue)
+			})
+
+			it('clamps & logs warning if the value received from setter is out of range', () => {
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
+				expect(param.value).toEqual(expectedValue)
+				expect(consoleWarnSpy).toHaveBeenCalledTimes(0)
+				param.value = lessThanMinValue
+				expect(param.value).toEqual(expectedMinValue)
+				expect(consoleWarnSpy).toHaveBeenCalledTimes(1)
+				expect(consoleWarnSpy).toHaveBeenCalledWith(`value ${lessThanMinValue} outside nominal range [${expectedMinValue}, ${expectedMaxValue}]; value will be clamped`)
+				param.value = moreThanMaxValue
+				expect(param.value).toEqual(expectedMaxValue)
+				expect(consoleWarnSpy).toHaveBeenCalledTimes(2)
+				expect(consoleWarnSpy).toHaveBeenCalledWith(`value ${moreThanMaxValue} outside nominal range [${expectedMinValue}, ${expectedMaxValue}]; value will be clamped`)
+			})
+
+			it('throws error if the value received from setter is not finite', () => {
+				const ctx: AudioContext = new AudioContext()
+				const {param}: CreatorFnResult = creator(ctx)
+				expect(param.value).toEqual(expectedValue)
+				expect(() => {
+					param.value = Infinity
+				}).toThrowErrorMatchingInlineSnapshot('"Failed to set the \'value\' property on \'AudioParam\': The provided float value is non-finite."')
+				expect(() => {
+					// @ts-expect-error bad input test
+					param.value = 'x'
+				}).toThrowErrorMatchingInlineSnapshot('"Failed to set the \'value\' property on \'AudioParam\': The provided float value is non-finite."')
+				expect(param.value).toEqual(expectedValue)
+			})
+
+			it('can be initialized from constructor', () => {
+				const ctx: AudioContext = new AudioContext()
+				const node: AudioNode = new globalThis[nodeName](ctx, {
+					[paramName]: differentInRangeValue,
+				})
+				// @ts-expect-error for testing
+				const param: AudioParam = (node[paramName] as AudioParam)
+				expect(param.value).toEqual(differentInRangeValue)
+			})
+
+			it('clamps & logs warning if the value received from constructor is out of range', () => {
+				const ctx: AudioContext = new AudioContext()
+				expect(consoleWarnSpy).toHaveBeenCalledTimes(0)
+				const node: AudioNode = new globalThis[nodeName](ctx, {
+					[paramName]: moreThanMaxValue,
+				})
+				// @ts-expect-error for testing
+				const param: AudioParam = (node[paramName] as AudioParam)
+				expect(consoleWarnSpy).toHaveBeenCalledTimes(1)
+				expect(consoleWarnSpy).toHaveBeenCalledWith(`value ${moreThanMaxValue} outside nominal range [${expectedMinValue}, ${expectedMaxValue}]; value will be clamped`)
+				expect(param.value).toEqual(expectedMaxValue)
+			})
+
+			it('throws error if the value received from constructor is not finite', () => {
+				const ctx: AudioContext = new AudioContext()
+				expect(() =>
+					new globalThis[nodeName](ctx, {
+						[paramName]: -Infinity,
+					}),
+				).toThrowErrorMatchingInlineSnapshot('"The provided float value is non-finite"')
+				expect(() =>
+					new globalThis[nodeName](ctx, {
+						[paramName]: 'x',
+					}),
+				).toThrowErrorMatchingInlineSnapshot('"The provided float value is non-finite"')
+			})
+		})
 
 		describe('"defaultValue" property', () => {
 			it(`equals to expectedDefaultValue (${expectedDefaultValue}) by default`, () => {
-				const {ctx, node, param}: CreatorFnResult = creator()
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
 				expect(param.defaultValue).toEqual(expectedDefaultValue)
 			})
 
 			it('cannot be modified', () => {
-				const {ctx, node, param}: CreatorFnResult = creator()
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
 				expect(param.defaultValue).toEqual(expectedDefaultValue)
 				// @ts-expect-error testing
-				param.defaultValue = expectedDefaultValue + 1
+				param.defaultValue = differentInRangeValue
 				expect(param.defaultValue).toEqual(expectedDefaultValue)
 			})
 		})
 
 		describe('"minValue" property', () => {
 			it(`equals to expectedMinValue (${expectedMinValue}) by default`, () => {
-				const {ctx, node, param}: CreatorFnResult = creator()
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
 				expect(param.minValue).toEqual(expectedMinValue)
 			})
 
 			it('cannot be modified', () => {
-				const {ctx, node, param}: CreatorFnResult = creator()
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
 				expect(param.minValue).toEqual(expectedMinValue)
 				// @ts-expect-error testing
-				param.minValue = expectedMinValue + (expectedMinValue * 0.25) // Add 25% of the value, so the test will be also reliable for huge values like GainNode.gain.minValue
+				param.minValue = differentInRangeValue
 				expect(param.minValue).toEqual(expectedMinValue)
 			})
 		})
 
 		describe('"maxValue" property', () => {
 			it(`equals to expectedMaxValue (${expectedMaxValue}) by default`, () => {
-				const {ctx, node, param}: CreatorFnResult = creator()
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
 				expect(param.maxValue).toEqual(expectedMaxValue)
 			})
 
 			it('cannot be modified', () => {
-				const {ctx, node, param}: CreatorFnResult = creator()
+				const ctx: AudioContext = new AudioContext()
+				const {node, param}: CreatorFnResult = creator(ctx)
 				expect(param.maxValue).toEqual(expectedMaxValue)
 				// @ts-expect-error testing
-				param.maxValue = expectedMaxValue + (expectedMinValue * 0.25) // Add 25% of the value, so the test will be also reliable for huge values like GainNode.gain.maxValue
+				param.maxValue = differentInRangeValue
 				expect(param.maxValue).toEqual(expectedMaxValue)
 			})
 		})
