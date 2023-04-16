72943dce9750f696dbac92a0577b7a6121cd76c0
George Satellite <lunaerxs@gmail.com>
2023-04-16 11:25:30 +0300

    coverage thresholds

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,10 @@ export default defineConfig({
 	test: {
 		coverage: {
 			reporter: ['text', 'json-summary', 'json'],
+      lines: 99,
+      branches: 99,
+      functions: 99,
+      statements: 99,
 		},
 	},
 })
